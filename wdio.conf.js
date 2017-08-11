d97eaa5a43a7ed9fc47858e475db89e0da5eb0ca
shylajanagineni <shylajanagineni@gmail.com>
2017-08-11 16:04:12 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -46,7 +46,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/fanatics.js'
+        'test/fanatics.js'
         ],
     // Patterns to exclude.
     //exclude: [
